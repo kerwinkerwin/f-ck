d64e5f21e85c96a6a4205481bdeb530e77af77d9
kerwinkerwin <kerwin@enspiral.com>
2015-07-01 21:24:57 +1200

    changed files

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
-HOSTNAME = "hello"
 
 module FCk
   class Application < Rails::Application
